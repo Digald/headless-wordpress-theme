be0fcd040635490367a722601255a32fd274a6cc
unknown <nosracer_gt@sbcglobal.net>
2018-11-25 22:19:51 -0600

    Set up react router and home page files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,18 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
+// Component Imports
+import Home from './Pages/Home/Home';
+
 class App extends Component {
-  state = {
-    movies: []
-  };
-
-  async componentDidMount() {
-    const dataURL =
-      "http://localhost/headless-wp/wordpress/wp-json/wp/v2/movies?_embed";
-    const getData = await axios.get(dataURL);
-    this.setState({
-      movies: getData.data
-    });
-  }
-
   render() {
-    let movies;
-    if (this.state.movies.length > 0) {
-      movies = this.state.movies.map((movie, index) => {
-        return (
-          <div key={index}>
-            <img
-              src={
-                movie._embedded["wp:featuredmedia"][0].media_details.sizes.large
-                  .source_url
-              }
-              alt="movie poster"
-            />
-            <p>
-              <strong>Title:</strong> {movie.title.rendered}
-            </p>
-            <p>
-              <strong>Release Year:</strong> {movie.acf.release_year}
-            </p>
-            <p>
-              <strong>Rating:</strong> {movie.acf.rating}
-            </p>
-            <p>
-              <strong>Description:</strong> {movie.acf.description}
-            </p>
-          </div>
-        );
-      });
-    }
-
     return (
-      <div>
-        <h2>Star Wars Movies</h2>
-        {movies}
-      </div>
+      <Router>
+        <Switch>
+          <Route extact path="/" component={Home} />
+        </Switch>
+      </Router>
     );
   }
 }
