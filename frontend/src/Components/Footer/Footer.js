e22b36a306cd3f2636027ebfb45c1c130f419479
unknown <nosracer_gt@sbcglobal.net>
2018-11-30 21:12:32 -0600

    Added links to fontawesome icons in footer to my person social media accounts

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -3,13 +3,27 @@ import "./Footer.css";
 
 class Footer extends Component {
   render() {
-    return <div className="Footer">
-      <div className="Footer__content">
-        <p>Headless Wordpress by Mark Alaniz</p>
-        <i className="fab fa-github-alt"></i>
-        <i className="fab fa-linkedin-in"></i>
+    return (
+      <div className="Footer">
+        <div className="Footer__content">
+          <p>Headless Wordpress by Mark Alaniz</p>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/Digald"
+          >
+            <i className="fab fa-github-alt" />
+          </a>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/markalaniz/"
+          >
+            <i className="fab fa-linkedin-in" />
+          </a>
+        </div>
       </div>
-    </div>;
+    );
   }
 }
 
